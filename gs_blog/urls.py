3a2b6d4ae551b51a032fb4bc7ebee83b8a5e3145
sk <2017meb1208@iitrpr.ac.in>
2020-02-25 07:18:28 +0530

    changes profile

--- gs_blog/urls.py
+++ gs_blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home,name='blog-home'),
+    #path('',views.about,name='blog-about'),
     path('about/',views.about,name='blog-about'),
     path('lgin/',views.lgin,name='blog-lgin')
 ]
